31fa3c23f5e152b7fb39997c4962d551eae4cb94
Lucy <lucydenisenko@gmail.com>
2022-05-09 15:42:18 -0700

    Initial commit

---
+++ src/Box.java
@@ -0,0 +1,39 @@
+public class Box {
+    private int a;
+    private int b;
+    private int c;
+
+    public Box(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public int calculateVolume() {
+        return a * b * c;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] arg){
+        Box box1 = new Box(2,3,4);
+        System.out.println(box1.calculateVolume());
+    }
+}
